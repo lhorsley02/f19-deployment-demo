37c9bf8ce5d309579dcea1acb9b97fc1487c5d99
Laura Horsley <horsleyshorses02@gmail.com>
2022-03-16 11:21:10 -0700

    Added CSS File

--- server/server.js
+++ server/server.js
@@ -7,7 +7,11 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../index.html")); 
+    res.sendFile(path.join(__dirname, "../public/index.html")); 
+});
+
+app.get("/styles", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.css"));
 });
 
 const port = process.env.PORT || 4005;
