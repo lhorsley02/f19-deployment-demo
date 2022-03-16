634feb1527c14ad5cc006e09d25fdb36e8caae57
Laura Horsley <horsleyshorses02@gmail.com>
2022-03-16 11:31:54 -0700

    Middleware Test

--- server/server.js
+++ server/server.js
@@ -6,13 +6,19 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html")); 
-});
+//Middleware
+app.use(express.static(path.join(__dirname, "../public")));
+app.use("/styles", express.static(path.join(__dirname, "../public/index.css")));
 
-app.get("/styles", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.css"));
-});
+
+// Endpoints
+// app.get("/", function(req, res) {
+//     res.sendFile(path.join(__dirname, "../public/index.html")); 
+// });
+
+// app.get("/styles", function(req, res) {
+//     res.sendFile(path.join(__dirname, "../public/index.css"));
+// });
 
 const port = process.env.PORT || 4005;
 
